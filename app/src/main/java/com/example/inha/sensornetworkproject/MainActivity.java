ada2b99b705facabb970de48a8251788309cd176
Sungwoo <ykk2006@naver.com>
2018-06-14 00:28:36 +0900

    rewrite api key

--- app/src/main/java/com/example/inha/sensornetworkproject/MainActivity.java
+++ app/src/main/java/com/example/inha/sensornetworkproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MapView mapView = new MapView(this);
-        mapView.setDaumMapApiKey("Your Key");
+        mapView.setDaumMapApiKey("e3c8162755026c8832fad1eda0bde41a");
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
